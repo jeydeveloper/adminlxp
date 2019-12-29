e24ffed255592bd9f618976c1abb147e0866fd66
andysociolla <andy.juliadi@sociolla.com>
2019-12-29 15:09:12 +0700

    add upload fitur

--- src/app/pages/home/content/ContentListPage.js
+++ src/app/pages/home/content/ContentListPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { Table } from "react-bootstrap";
 import {
   Portlet,
@@ -8,7 +8,13 @@ import {
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 import {
-  TextField
+  TextField,
+  Select,
+  Chip,
+  MenuItem,
+  Input,
+  FormControl,
+  InputLabel
 } from "@material-ui/core";
 import { Alert } from "react-bootstrap";
 import axios from "axios";
@@ -19,84 +25,174 @@ const useStyles = makeStyles(theme => ({
   button: {
     margin: theme.spacing(1),
   },
+  formControl: {
+    minWidth: 300,
+    maxWidth: 400,
+    marginTop: "16px",
+    marginRight: "8px"
+  },
+  chips: {
+    display: "flex",
+    flexWrap: "wrap"
+  },
+  chip: {
+    margin: 2
+  }
 }));
 
 const style = {
   rowTable: {"verticalAlign":"middle"},
   fullWidth: {width: "100%"},
   textAlignCenter: {"textAlign": "center"},
+  marginTopBottom: {
+    marginTop: "16px", 
+    marginBottom: "8px"
+  }
 };
                   
 export default function ContentListPage() {
     const classes = useStyles();
 
     const initialStateForm = {
-      "fullname": "",
-      "username": "",
-      "email": "",
-      "password": "",
+      "title": "",
+      "description": "",
+      "type": "",
+      "attribute": [],
     };
+
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+      PaperProps: {
+        style: {
+          maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+          width: 250
+        }
+      }
+    };
+
     const [success, setSuccess] = useState(null);
+    const [failed, setFailed] = useState(null);
     const [form, setForm] = useState(null);
-    const [users, setUsers] = useState([]);
+    const [contents, setContents] = useState([]);
     const [values, setValues] = useState(initialStateForm);
     const [formId, setFormId] = useState(false);
+    const [attributes, setAttributes] = useState([]);
+    const [attributeValue, setAttributeValue] = useState([]);
+    const [images, setImages] = useState(null);
 
     useEffect(() => {
       async function fetchData() {
-        axios.get(`${REACT_APP_API_URL}/users`)
+        axios.get(`${REACT_APP_API_URL}/contents`)
         .then(res => {
           // console.log(res);
-          setUsers(res.data)
+          setContents(res.data)
         })
       }
       fetchData();
+      async function fetchDataAttribute() {
+        axios.get(`${REACT_APP_API_URL}/attributes`)
+        .then(res => {
+          setAttributes(res.data.filter(value => value.show_for === "content"));
+        })
+      }
+      fetchDataAttribute();
     }, []);
 
-    const editUser = (user) => {
-      setValues({ ...values, ...user });
+    const editContent = (content) => {
+      setValues({ ...values, ...content });
       setForm(true);
-      setFormId(user._id);
+      setFormId(content._id);
+      setAttributeValue(content.attribute.map(value => value.attribute_value));
     };
 
-    const deleteUser = (user) => {
-      axios.delete(`${REACT_APP_API_URL}/users/${user._id}`)
+    const deleteContent = (content) => {
+      axios.delete(`${REACT_APP_API_URL}/contents/${content._id}`)
       .then(res => {
-        setUsers(users.filter(value => value.no !== user._id));
+        setContents(contents.filter(value => value._id !== content._id));
       })
     };
     
     const handleChange = name => event => {
-      setValues({ ...values, [name]: event.target.value });
+      if(name === "upload") {
+        const files = event.target.files[0];
+        console.log(files);
+        setImages(files);
+      } else {
+        setValues({ ...values, [name]: event.target.value });
+      }
     };
 
-    const addUser = () => {
-      setValues({ ...values, ...initialStateForm });
+    const handleAttribute = index => event => {
+        let newAttributeValue = [...attributeValue];
+        newAttributeValue[index] = event.target.value;
+        setAttributeValue(newAttributeValue);
+    };
+
+    const addContent = () => {
+      setValues(initialStateForm);
       setForm(true);
     };
 
     const saveForm = () => {
+      setSuccess(false);
+      setFailed(false);
+      values.attribute = attributes.map((valAttribute, idxAttribute) => ({
+      "attribute_name":valAttribute.name,
+      "attribute_value":(attributeValue[idxAttribute] || "")
+      }));
       if(formId) {
-        axios.put(`${REACT_APP_API_URL}/users/${formId}`, values)
+        axios.put(`${REACT_APP_API_URL}/contents/${formId}`, values)
         .then(res => {
-          setUsers(users.map(value => (value._id === formId ? res.data : value)));
+          setContents(contents.map(value => (value._id === formId ? res.data : value)));
           setSuccess(true);
         })
       } else {
-        axios.post(`${REACT_APP_API_URL}/users`, values)
-        .then(res => {
-          values._id = res.data._id;
-          setUsers([...users, values]);
-          setValues({ ...values, ...initialStateForm });
-          setSuccess(true);
-        })
+        if(images) {
+          const data = new FormData();
+          data.append('file', images)
+          axios.post(`${REACT_APP_API_URL}/upload`, data)
+          .then(res => {
+            console.log(res);
+            return axios.post(`${REACT_APP_API_URL}/contents`, values);
+          })
+          .then(res => {
+            values._id = res.data._id;
+            setContents([...contents, values]);
+            setValues({ ...values, ...initialStateForm });
+            setSuccess(true);
+            clearForm();
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+        } else {
+          axios.post(`${REACT_APP_API_URL}/contents`, values)
+          .then(res => {
+            values._id = res.data._id;
+            setContents([...contents, values]);
+            setValues({ ...values, ...initialStateForm });
+            setSuccess(true);
+            clearForm();
+          })
+          .catch(function (error) {
+            setFailed(true);
+          })
+        }
       }
     };
 
     const doneForm = () => {
       setForm(false);
       setSuccess(false);
+      setFailed(false);
       setFormId(null);
+      clearForm();
+    };
+
+    const clearForm = () => {
+        setAttributeValue([]);
+        setImages(null);
     };
 
     return (
@@ -105,49 +201,123 @@ export default function ContentListPage() {
           <div className="row">
             <div className="col-md-12">
             {success && (
-            <Alert style={{"margin-top":"10px"}} variant="success">
+            <Alert style={{"marginTop":"10px"}} variant="success">
               Save data success!
+            </Alert>
+            )}
+            {failed && (
+            <Alert style={{"marginTop":"10px"}} variant="danger">
+              Save data failed!
             </Alert>
             )}
             <Portlet>
               <PortletBody fluid={true}>
                 <form style={style.fullWidth} noValidate autoComplete="off">
                   <TextField
-                    id="standard-fullname"
-                    label="Fullname"
-                    value={values.fullname}
-                    onChange={handleChange("fullname")}
+                    id="standard-title"
+                    label="Title"
+                    value={values.title}
+                    onChange={handleChange("title")}
                     margin="normal"
                     fullWidth
                     required
                   />
                   <TextField
-                    id="standard-username"
-                    label="Username"
-                    value={values.username}
-                    onChange={handleChange("username")}
+                    id="standard-description"
+                    label="Description"
+                    value={values.description}
+                    onChange={handleChange("description")}
                     margin="normal"
                     fullWidth
                     required
                   />
                   <TextField
-                    id="standard-email"
-                    label="Email"
-                    value={values.email}
-                    onChange={handleChange("email")}
+                    id="standard-type"
+                    label="Type"
+                    value={values.type}
+                    onChange={handleChange("type")}
                     margin="normal"
                     fullWidth
                     required
                   />
-                  <TextField
-                    id="standard-password"
-                    label="Password"
-                    value={values.password}
-                    onChange={handleChange("password")}
-                    margin="normal"
-                    fullWidth
-                    required
-                  />
+                  <input type="file" name="file" onChange={handleChange("upload")}/>
+                  {
+                    attributes.map((valAttribute, idxAttribute) => 
+                        <Fragment key={`contentattribute${idxAttribute}`}>
+                        {valAttribute.type === "date" && 
+                        <TextField
+                            value={attributeValue[idxAttribute] || ""}
+                            type="date"
+                            label={valAttribute.name}
+                            onChange={handleAttribute(idxAttribute)}
+                            margin="normal"
+                            fullWidth
+                            InputLabelProps={{
+                                shrink: true
+                            }}
+                        />
+                        }
+                        {valAttribute.type === "text" && 
+                        <TextField
+                            value={attributeValue[idxAttribute] || ""}
+                            label={valAttribute.name}
+                            onChange={handleAttribute(idxAttribute)}
+                            margin="normal"
+                            fullWidth
+                        />
+                        }
+                        {valAttribute.type === "list" && 
+                        <div>
+                        <FormControl className={classes.formControl}>
+                        <InputLabel>{valAttribute.name}</InputLabel>
+                        <Select
+                          key={`sel2${idxAttribute}`}
+                          value={attributeValue[idxAttribute] || ""}
+                          onChange={handleAttribute(idxAttribute)}
+                          displayEmpty
+                          name="attributevalue"
+                        >
+                          {valAttribute.value.map((valAttributeValue, indexAttributeValue) =>
+                            <MenuItem key={`attr2${indexAttributeValue}`} value={valAttributeValue}>{valAttributeValue}</MenuItem>
+                          )}
+                        </Select>
+                        </FormControl>
+                        </div>
+                        }
+                        {valAttribute.type === "multiselect" && 
+                        <div>
+                        <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="select-multiple-chip">{valAttribute.name}</InputLabel>
+                        <Select
+                          multiple
+                          key={`sel2${idxAttribute}`}
+                          value={attributeValue[idxAttribute] || []}
+                          onChange={handleAttribute(idxAttribute)}
+                          name="attributevalue"
+                          input={<Input id={`select-multiple-chip-${idxAttribute}`} />}
+                          renderValue={selected => (
+                            <div className={classes.chips}>
+                              {selected.map(value => (
+                                <Chip
+                                  key={value}
+                                  label={value}
+                                  className={classes.chip}
+                                />
+                              ))}
+                            </div>
+                          )}
+                          MenuProps={MenuProps}
+                        >
+                          {valAttribute.value.map((valAttributeValue, indexAttributeValue) =>
+                            <MenuItem key={`attr2${indexAttributeValue}`} value={valAttributeValue}>{valAttributeValue}</MenuItem>
+                          )}
+                        </Select>
+                        </FormControl>
+                        </div>
+                        }
+                        </Fragment>
+                    )
+                  }
                 </form>
               </PortletBody>
             </Portlet>
@@ -170,10 +340,10 @@ export default function ContentListPage() {
           <div className="col-md-12">
             <Portlet>
               <PortletHeader
-                title="List User"
+                title="List Content"
                 toolbar={
-                  <Button onClick={addUser} color="primary" variant="contained" className={classes.button}>
-                    Add User
+                  <Button onClick={addContent} color="primary" variant="contained" className={classes.button}>
+                    Add Content
                   </Button>
                 }
               />
@@ -182,32 +352,32 @@ export default function ContentListPage() {
                   <thead>
                     <tr>
                       <th>#</th>
-                      <th>Fullname</th>
-                      <th>Username</th>
-                      <th>Email</th>
+                      <th>Title</th>
+                      <th>Description</th>
+                      <th>Type</th>
                       <th>Action</th>
                     </tr>
                   </thead>
                   <tbody>
                     {
-                      users.map((value, index) => (
-                        <tr key={index}>
+                      contents.map((value, index) => {
+                        return <tr key={index}>
                           <td style={style.rowTable}>{(index + 1)}</td>
-                          <td style={style.rowTable}>{value.fullname}</td>
-                          <td style={style.rowTable}>{value.username}</td>
-                          <td style={style.rowTable}>{value.email}</td>
+                          <td style={style.rowTable}>{value.title}</td>
+                          <td style={style.rowTable}>{value.description}</td>
+                          <td style={style.rowTable}>{value.type}</td>
                           <td>
-                            <Button onClick={() => {editUser(value)}} color="primary" variant="contained" className={classes.button}>
+                            <Button onClick={() => {editContent(value)}} color="primary" variant="contained" className={classes.button}>
                               Edit
                             </Button>
-                             <Button onClick={() => {deleteUser(value)}} variant="contained" className={classes.button}>
+                             <Button onClick={() => {deleteContent(value)}} variant="contained" className={classes.button}>
                               Delete
                             </Button>
                           </td>
                         </tr>
-                      ))
+                      })
                     }
-                    {users.length === 0 && (
+                    {contents.length === 0 && (
                       <tr>
                         <td colSpan="5">Data is empty</td>
                       </tr>
