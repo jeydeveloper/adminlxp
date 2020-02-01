969eb00a5923d85b881b421862cbd5254f45d6ea
Jey Developer <jey.developer@google.com>
2020-02-01 22:06:43 +0700

    update

--- src/app/pages/home/User.js
+++ src/app/pages/home/User.js
@@ -43,105 +43,105 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const style = {
-  rowTable: {"verticalAlign":"middle"},
-  fullWidth: {width: "100%"},
-  textAlignCenter: {"textAlign": "center"},
+  rowTable: { "verticalAlign": "middle" },
+  fullWidth: { width: "100%" },
+  textAlignCenter: { "textAlign": "center" },
   marginTopBottom: {
-    marginTop: "16px", 
+    marginTop: "16px",
     marginBottom: "8px"
   }
 };
-                  
+
 function User() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const initialStateForm = {
-      "fullname": "",
-      "username": "",
-      "email": "",
-      "password": "",
-      "attribute": [],
-    };
+  const initialStateForm = {
+    "fullname": "",
+    "username": "",
+    "email": "",
+    "password": "",
+    "attribute": [],
+  };
 
-    const ITEM_HEIGHT = 48;
-    const ITEM_PADDING_TOP = 8;
-    const MenuProps = {
-      PaperProps: {
-        style: {
-          maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-          width: 250
-        }
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250
       }
-    };
+    }
+  };
 
-    const [success, setSuccess] = useState(null);
-    const [failed, setFailed] = useState(null);
-    const [form, setForm] = useState(null);
-    const [users, setUsers] = useState([]);
-    const [values, setValues] = useState(initialStateForm);
-    const [formId, setFormId] = useState(false);
-    const [attributes, setAttributes] = useState([]);
-    const [attributeValue, setAttributeValue] = useState([]);
-    const [message, setMessage] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [failed, setFailed] = useState(null);
+  const [form, setForm] = useState(null);
+  const [users, setUsers] = useState([]);
+  const [values, setValues] = useState(initialStateForm);
+  const [formId, setFormId] = useState(false);
+  const [attributes, setAttributes] = useState([]);
+  const [attributeValue, setAttributeValue] = useState([]);
+  const [message, setMessage] = useState(null);
 
-    useEffect(() => {
-      async function fetchData() {
-        axios.get(`${REACT_APP_API_URL}/users`)
+  useEffect(() => {
+    async function fetchData() {
+      axios.get(`${REACT_APP_API_URL}/users`)
         .then(res => {
           // console.log(res);
           setUsers(res.data)
         })
-      }
-      fetchData();
-      async function fetchDataAttribute() {
-        axios.get(`${REACT_APP_API_URL}/attributes`)
+    }
+    fetchData();
+    async function fetchDataAttribute() {
+      axios.get(`${REACT_APP_API_URL}/attributes`)
         .then(res => {
           setAttributes(res.data.filter(value => value.show_for_user));
         })
-      }
-      fetchDataAttribute();
-    }, []);
+    }
+    fetchDataAttribute();
+  }, []);
 
-    const editUser = (user) => {
-      setValues({ ...values, ...user });
-      setForm(true);
-      setFormId(user._id);
-      setAttributeValue(user.attribute.map(value => value.attribute_value));
-    };
+  const editUser = (user) => {
+    setValues({ ...values, ...user });
+    setForm(true);
+    setFormId(user._id);
+    setAttributeValue(user.attribute.map(value => value.attribute_value));
+  };
 
-    const deleteUser = (user) => {
-      axios.delete(`${REACT_APP_API_URL}/users/${user._id}`)
+  const deleteUser = (user) => {
+    axios.delete(`${REACT_APP_API_URL}/users/${user._id}`)
       .then(res => {
         setUsers(users.filter(value => value._id !== user._id));
       })
-    };
-    
-    const handleChange = name => event => {
-      setFailed(false);
-      setMessage(null);
-      setValues({ ...values, [name]: event.target.value });
-    };
+  };
 
-    const handleAttribute = index => event => {
-      setFailed(false);
-      setMessage(null);
-        let newAttributeValue = [...attributeValue];
-        newAttributeValue[index] = event.target.value;
-        setAttributeValue(newAttributeValue);
-    };
+  const handleChange = name => event => {
+    setFailed(false);
+    setMessage(null);
+    setValues({ ...values, [name]: event.target.value });
+  };
 
-    const addUser = () => {
-      setValues({ ...values, ...initialStateForm });
-      setForm(true);
-    };
+  const handleAttribute = index => event => {
+    setFailed(false);
+    setMessage(null);
+    let newAttributeValue = [...attributeValue];
+    newAttributeValue[index] = event.target.value;
+    setAttributeValue(newAttributeValue);
+  };
 
-    const saveForm = () => {
-        values.attribute = attributes.map((valAttribute, idxAttribute) => ({
-        "attribute_name":valAttribute.name,
-        "attribute_value":(attributeValue[idxAttribute] || "")
-        }));
-      if(formId) {
-        axios.put(`${REACT_APP_API_URL}/users/${formId}`, values)
+  const addUser = () => {
+    setValues({ ...values, ...initialStateForm });
+    setForm(true);
+  };
+
+  const saveForm = () => {
+    values.attribute = attributes.map((valAttribute, idxAttribute) => ({
+      "attribute_name": valAttribute.name,
+      "attribute_value": (attributeValue[idxAttribute] || "")
+    }));
+    if (formId) {
+      axios.put(`${REACT_APP_API_URL}/users/${formId}`, values)
         .then(res => {
           setUsers(users.map(value => (value._id === formId ? res.data : value)));
           setSuccess(true);
@@ -150,8 +150,8 @@ function User() {
           setFailed(true);
           setMessage(err.message);
         })
-      } else {
-        axios.post(`${REACT_APP_API_URL}/users`, values)
+    } else {
+      axios.post(`${REACT_APP_API_URL}/users`, values)
         .then(res => {
           values._id = res.data._id;
           setUsers([...users, values]);
@@ -163,34 +163,35 @@ function User() {
           setFailed(true);
           setMessage(err.message);
         })
-      }
-    };
+    }
+  };
 
-    const doneForm = () => {
-      setForm(false);
-      setSuccess(false);
-      setFormId(null);
-      clearForm();
-    };
+  const doneForm = () => {
+    setForm(false);
+    setSuccess(false);
+    setFormId(null);
+    clearForm();
+  };
 
-    const clearForm = () => {
-        setAttributeValue([]);
-    };
+  const clearForm = () => {
+    setValues(initialStateForm);
+    setAttributeValue([]);
+  };
 
-    return (
-      <>
-        {form && 
-          <div className="row">
-            <div className="col-md-12">
+  return (
+    <>
+      {form &&
+        <div className="row">
+          <div className="col-md-12">
             {success && (
-            <Alert style={{"marginTop":"10px"}} variant="success">
-              Save data success!
+              <Alert style={{ "marginTop": "10px" }} variant="success">
+                Save data success!
             </Alert>
             )}
             {failed && (
-            <Alert style={{"marginTop":"10px"}} variant="danger">
-              {message}
-            </Alert>
+              <Alert style={{ "marginTop": "10px" }} variant="danger">
+                {message}
+              </Alert>
             )}
             <Portlet>
               <PortletBody fluid={true}>
@@ -222,25 +223,23 @@ function User() {
                     fullWidth
                     required
                   />
+
+                  <TextField
+                    id="standard-password"
+                    type="password"
+                    label={<FormattedMessage id="USER.PASSWORD" />}
+                    value={values.password}
+                    onChange={handleChange("password")}
+                    margin="normal"
+                    fullWidth
+                    required
+                  />
+
                   {
-                      !formId && 
-                      <TextField
-                        id="standard-password"
-                        type="password"
-                        label={<FormattedMessage id="USER.PASSWORD" />}
-                        value={values.password}
-                        onChange={handleChange("password")}
-                        margin="normal"
-                        fullWidth
-                        required
-                    />
-                  }
-                  
-                  {
-                    attributes.map((valAttribute, idxAttribute) => 
-                        <Fragment key={`userattribute${idxAttribute}`}>
-                        {valAttribute.type === "date" && 
-                        <TextField
+                    attributes.map((valAttribute, idxAttribute) =>
+                      <Fragment key={`userattribute${idxAttribute}`}>
+                        {valAttribute.type === "date" &&
+                          <TextField
                             value={attributeValue[idxAttribute] || ""}
                             type="date"
                             label={valAttribute.name}
@@ -248,89 +247,89 @@ function User() {
                             margin="normal"
                             fullWidth
                             InputLabelProps={{
-                                shrink: true
+                              shrink: true
                             }}
-                        />
+                          />
                         }
-                        {valAttribute.type === "text" && 
-                        <TextField
+                        {valAttribute.type === "text" &&
+                          <TextField
                             value={attributeValue[idxAttribute] || ""}
                             label={valAttribute.name}
                             onChange={handleAttribute(idxAttribute)}
                             margin="normal"
                             fullWidth
-                        />
+                          />
                         }
-                        {valAttribute.type === "list" && 
-                        <div>
-                        <FormControl className={classes.formControl}>
-                        <InputLabel>{valAttribute.name}</InputLabel>
-                        <Select
-                          key={`sel2${idxAttribute}`}
-                          value={attributeValue[idxAttribute] || ""}
-                          onChange={handleAttribute(idxAttribute)}
-                          displayEmpty
-                          name="attributevalue"
-                        >
-                          {valAttribute.value.map((valAttributeValue, indexAttributeValue) =>
-                            <MenuItem key={`attr2${indexAttributeValue}`} value={valAttributeValue}>{valAttributeValue}</MenuItem>
-                          )}
-                        </Select>
-                        </FormControl>
-                        </div>
+                        {valAttribute.type === "list" &&
+                          <div>
+                            <FormControl className={classes.formControl}>
+                              <InputLabel>{valAttribute.name}</InputLabel>
+                              <Select
+                                key={`sel2${idxAttribute}`}
+                                value={attributeValue[idxAttribute] || ""}
+                                onChange={handleAttribute(idxAttribute)}
+                                displayEmpty
+                                name="attributevalue"
+                              >
+                                {valAttribute.value.map((valAttributeValue, indexAttributeValue) =>
+                                  <MenuItem key={`attr2${indexAttributeValue}`} value={valAttributeValue}>{valAttributeValue}</MenuItem>
+                                )}
+                              </Select>
+                            </FormControl>
+                          </div>
                         }
-                        {valAttribute.type === "multiselect" && 
-                        <div>
-                        <FormControl className={classes.formControl}>
-                        <InputLabel htmlFor="select-multiple-chip">{valAttribute.name}</InputLabel>
-                        <Select
-                          multiple
-                          key={`sel2${idxAttribute}`}
-                          value={attributeValue[idxAttribute] || []}
-                          onChange={handleAttribute(idxAttribute)}
-                          name="attributevalue"
-                          input={<Input id={`select-multiple-chip-${idxAttribute}`} />}
-                          renderValue={selected => (
-                            <div className={classes.chips}>
-                              {selected.map(value => (
-                                <Chip
-                                  key={value}
-                                  label={value}
-                                  className={classes.chip}
-                                />
-                              ))}
-                            </div>
-                          )}
-                          MenuProps={MenuProps}
-                        >
-                          {valAttribute.value.map((valAttributeValue, indexAttributeValue) =>
-                            <MenuItem key={`attr2${indexAttributeValue}`} value={valAttributeValue}>{valAttributeValue}</MenuItem>
-                          )}
-                        </Select>
-                        </FormControl>
-                        </div>
+                        {valAttribute.type === "multiselect" &&
+                          <div>
+                            <FormControl className={classes.formControl}>
+                              <InputLabel htmlFor="select-multiple-chip">{valAttribute.name}</InputLabel>
+                              <Select
+                                multiple
+                                key={`sel2${idxAttribute}`}
+                                value={attributeValue[idxAttribute] || []}
+                                onChange={handleAttribute(idxAttribute)}
+                                name="attributevalue"
+                                input={<Input id={`select-multiple-chip-${idxAttribute}`} />}
+                                renderValue={selected => (
+                                  <div className={classes.chips}>
+                                    {selected.map(value => (
+                                      <Chip
+                                        key={value}
+                                        label={value}
+                                        className={classes.chip}
+                                      />
+                                    ))}
+                                  </div>
+                                )}
+                                MenuProps={MenuProps}
+                              >
+                                {valAttribute.value.map((valAttributeValue, indexAttributeValue) =>
+                                  <MenuItem key={`attr2${indexAttributeValue}`} value={valAttributeValue}>{valAttributeValue}</MenuItem>
+                                )}
+                              </Select>
+                            </FormControl>
+                          </div>
                         }
-                        </Fragment>
+                      </Fragment>
                     )
                   }
                 </form>
               </PortletBody>
             </Portlet>
-            </div>
-            <div className="col-md-12">
-              <div style={style.textAlignCenter}>
-                <Button onClick={saveForm} color="primary" variant="contained" className={classes.button}>
-                  Save Data
+          </div>
+          <div className="col-md-12">
+            <div style={style.textAlignCenter}>
+              <Button onClick={saveForm} color="primary" variant="contained" className={classes.button}>
+                Save Data
                 </Button>
-                <Button onClick={doneForm} variant="contained" className={classes.button}>
-                  Done
+              <Button onClick={doneForm} variant="contained" className={classes.button}>
+                Done
                 </Button>
-              </div>
             </div>
           </div>
-        }
+        </div>
+      }
 
-        {!form && 
+      {!form &&
         <div className="row">
           <div className="col-md-12">
             <Portlet>
@@ -362,10 +361,10 @@ function User() {
                           <td style={style.rowTable}>{value.username}</td>
                           <td style={style.rowTable}>{value.email}</td>
                           <td>
-                            <Button onClick={() => {editUser(value)}} color="primary" variant="contained" className={classes.button}>
+                            <Button onClick={() => { editUser(value) }} color="primary" variant="contained" className={classes.button}>
                               Edit
                             </Button>
-                             <Button onClick={() => {deleteUser(value)}} variant="contained" className={classes.button}>
+                            <Button onClick={() => { deleteUser(value) }} variant="contained" className={classes.button}>
                               Delete
                             </Button>
                           </td>
@@ -383,9 +382,9 @@ function User() {
             </Portlet>
           </div>
         </div>
-        }
-      </>
-    );
+      }
+    </>
+  );
 }
 
 export default injectIntl(
